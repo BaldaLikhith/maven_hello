9321dff72f7d914a97511ee386490a25738a78f2
likhith <baldasailikhith@gamil.com>
2023-12-16 10:39:11 +0530

    demmoo

---
+++ src/main/java/com/app/demo/likhith_1/App.java
@@ -0,0 +1,14 @@
+package com.app.demo.likhith_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!_likhith" );
+        
+    }
+}
